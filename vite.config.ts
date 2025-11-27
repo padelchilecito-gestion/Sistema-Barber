ee20f69d4725155c185e66d681dc7aac536ef7e2
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-27 15:30:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    // Carga las variables de entorno para que estén disponibles si se necesitan en la configuración
     const env = loadEnv(mode, '.', '');
     return {
       server: {
@@ -10,10 +11,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      // Hemos eliminado el bloque "define" porque ya usamos el método nativo import.meta.env
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
