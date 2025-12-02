b26d75b3a7aee120010580164c2c22e7d0e4c58f
padelclilecito-gestion <padelchilecito@gmail.com>
2025-12-02 12:43:07 -0300

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,13 @@
+// config.ts
+
+export const PROJECT_STATUS = {
+  // --- INTERRUPTOR PRINCIPAL ---
+  // true = El sitio funciona normal
+  // false = Muestra la pantalla de bloqueo (falta de pago)
+  isActive: true, 
+  
+  // Datos que verá tu cliente
+  providerName: "Cominucate conmigo para reguralizar la cuenta  ", 
+  providerWhatsapp: "3825625422", // Tu número real
+  providerEmail: "edur900@gmail.com"
+};
